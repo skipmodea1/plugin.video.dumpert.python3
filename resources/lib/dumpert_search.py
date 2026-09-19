5c97c18a2874a958e48fb8f4a25b6fe7f14207f2
SkipmodeA1 <skipmodea1@outlook.com>
2026-09-19 11:41:16 +0200

    improved search url construction

--- resources/lib/dumpert_search.py
+++ resources/lib/dumpert_search.py
@@ -6,14 +6,13 @@
 #
 from future import standard_library
 standard_library.install_aliases()
-from builtins import str
 from builtins import object
 import sys
 import xbmc
 import urllib.parse
 
-from resources.lib.dumpert_const import SEARCH_URL_PART_2, SEARCH_URL_PART_3, INITIAL_PAGE_NUMBER, LANGUAGE, \
-    log, convertToUnicodeString
+from resources.lib.dumpert_const import SEARCH_URL_PART_1, SEARCH_URL_PART_2, SEARCH_URL_PART_3, \
+    INITIAL_PAGE_NUMBER, LANGUAGE, log
 
 
 #
@@ -43,24 +42,32 @@ class Main(object):
             # If the user cancels the input box, we stop
             sys.exit(0)
 
+        # We are going to update the value of the key 'url' the system parameters, ie after 'url=' and before next key
+        # ?action=search&next_page_possible=True&plugin_category=Search&url=https%3a%2f%2fpost.dumpert.nl%2fapi%2fv1.0%2fsearch%2f
+        # An url decoded search url should look like this:
+        # https://post.dumpert.nl/api/v1.0/search/<user entered search term>/0/?order=date&media_type=all&app=www.dumpert.nl"
+
         #log("sys.argv[2]-1", sys.argv[2])
 
-        # Constructing last part of search url in the parameters, i.e. the part after the parameters in sys.argv[2]
-        # so after this: ?action=search&next_page_possible=True&plugin_category=Search&url=https%3a%2f%2fpost.dumpert.nl%2fapi%2fv1.0%2fsearch%2f
-        # an url decoded search url should look like this: 
-        # https://post.dumpert.nl/api/v1.0/search/<user entered search term>/0/?order=date&media_type=all&app=www.dumpert.nl"
-        urldecoded_last_part_search_url = search_term + SEARCH_URL_PART_2 + INITIAL_PAGE_NUMBER + SEARCH_URL_PART_3
-        
-        #log("urldecoded_last_part_search_url", urldecoded_last_part_search_url)
-        
-        # Url encode the url string
-        urlencoded_last_part_search_url = urllib.parse.quote(urldecoded_last_part_search_url, safe='')
+        # Convert the parameter string to a dictionary
+        parameter_dictionary = urllib.parse.parse_qs(sys.argv[2])
 
-        #log("urlencoded_last_part_search_url", urlencoded_last_part_search_url)
-        
-        sys.argv[2] = sys.argv[2] + urlencoded_last_part_search_url
+        #log("parameter_dictionary-1", parameter_dictionary)
 
-        #log("sys.argv[2]-2", sys.argv[2])
+        # Update value of key 'url', remove '/' from search term to prevent decoding errors
+        parameter_dictionary['url'] = [SEARCH_URL_PART_1 + urllib.parse.quote(search_term.replace('/','')) + SEARCH_URL_PART_2 + INITIAL_PAGE_NUMBER + SEARCH_URL_PART_3]
+
+        #log("parameter_dictionary-2", parameter_dictionary)
+
+        # Convert to a flat dictionary with first item from each list
+        flat_parameter_dictionary = {k: v[0] for k, v in parameter_dictionary.items()}
+
+        #log("flat_parameter_dictionary", flat_parameter_dictionary)
+
+        # Encode the URL parameters
+        sys.argv[2] = urllib.parse.urlencode(flat_parameter_dictionary, safe='?')
+
+        log("sys.argv[2]-2", sys.argv[2])
 
         import resources.lib.dumpert_json as plugin
 
